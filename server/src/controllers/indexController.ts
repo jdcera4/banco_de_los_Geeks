ce60c749529b2e319ff64dec2b07f56526dc4522
jdcera4 <jdcera4@gmail.com>
2022-03-07 15:27:29 -0500

    update logic fibonacci

--- server/src/controllers/indexController.ts
+++ server/src/controllers/indexController.ts
@@ -1,31 +1,20 @@
 import { Request, Response } from 'express';
+import { QueryResult } from 'pg';
 import pool from '../db';
 
 class IndexController {
 
     public async create(req: Request, res: Response): Promise<object> {
-        let value1 = req.body.value1;
-        let value2 = req.body.value2;
+        let value1 = parseInt(req.body.value1);
+        let value2 = parseInt(req.body.value2);
         let result = value1 + value2;
+        let fibo = [0, 1, 1, 2, 3, 5];
 
-        let a = 0;
-        let b = 1;
-        let bol = false;
-        if(result === 0){
-            bol = true;
+        for(let i = 6; i <= result; i++){
+            fibo.push(fibo[i-1] + fibo[i-2]);
         }
-
-        for(let i = 1; i <= result; i++){
-            if(result === a){
-                bol = true;
-            }
-            let c = a+b;
-            a=b;
-            b=c;
-        }
-        const games = await pool.query('SELECT * FROM result_sum');
-        console.log(games);
-        return res.json({ fibonacci: bol, result: result });
+        let fibonacci = fibo.includes(result);
+        return res.json({ fibonacci: fibonacci, result: result });
     }
 
 }
--- server/src/db.ts
+++ server/src/db.ts
@@ -1,10 +1,24 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  user: "postgres",
+  user: "david",
   host: "localhost",
-  password: "",
-  database: "firstapi",
+  password: "admin",
+  database: "db_geeks",
   port: 5432,
 });
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack)
+  }
+  client.query('SELECT NOW()', (err, result) => {
+    release()
+    if (err) {
+      return console.error('Error executing query', err.stack)
+    }
+    console.log(result.rows)
+  })
+})
+
 export default pool;
