dc47d2ac75b9d4e9b210c786ec1e71cc643c227f
jdcera4 <jdcera4@gmail.com>
2022-03-07 15:27:50 -0500

    create conect api

--- web/src/App.js
+++ web/src/App.js
@@ -1,41 +1,50 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import { FloatingLabel, Form, Card, Button } from 'react-bootstrap';
-import axios from 'axios';
+import { FloatingLabel, Form, Card, Button, Alert } from 'react-bootstrap';
 
 function App() {
-  const [articleId, setArticleId] = useState(null);
-  let datos = {
-    value1: 1,
-    value2: 2
-  }
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const [result, setResult] = useState(0);
+  const [fibonacci, setFibonacci] = useState(false);
 
-  useEffect(() => {
-    // POST request using axios inside useEffect React hook
-    axios.post('http://localhost:3000', datos)
-        .then(response => setArticleId(response));
-
-// empty dependency array means this effect will only run once (like componentDidMount in classes)
-}, []);
-
-  onsubmit = e => {
-    e.onsubmit();
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+        await fetch("http://localhost:3000/", {
+           method: "POST",
+           headers: {
+               "Content-Type": "application/json",
+           },
+           body: JSON.stringify({
+               value1: value1,
+               value2: value2, 
+           }),
+       }).then(response => response.json()).then(data => {
+         setFibonacci(data.fibonacci);
+         setResult(data.result);
+       });
+      //  window.location = "/";
+    } catch (err) {
+       console.error(err.message);
+    }
   };
 
   return (
     <Card style={{ width: '30rem', margin: '50px auto', padding: '20px' }}>
-      <form onSubmit={onsubmit}>
+      <Alert><p>Result:{result}</p><p>it's in fibonacci: {fibonacci ? 'si' : 'no'}</p></Alert>
+      <form onSubmit={onSubmitForm}>
         <FloatingLabel
           controlId="floatingInput"
           label="Enter Number"
           className="mb-3"
         >
-          <Form.Control type="number" placeholder="NUmber 1" />
+          <Form.Control type="number" placeholder="Number 1" name="value1" value={value1} onChange={e => setValue1(e.target.value)}/>
         </FloatingLabel>
         <FloatingLabel controlId="floatingPassword" label="Enter Number">
-          <Form.Control type="number" placeholder="Number 2" />
+          <Form.Control type="number" placeholder="Number 2" name="value2" value={value2} onChange={e => setValue2(e.target.value)}/>
         </FloatingLabel>
-        <Button style={{marginTop: '10px'}}>Submit</Button>
+        <Button style={{marginTop: '10px'}} type='submit'>Submit</Button>
       </form>
     </Card>
   );
